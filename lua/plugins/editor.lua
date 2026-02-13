518c2a566d08a665fec2d2340d2e67992cbdef45
yibhou <richard.lamn@hotmail.com>
2026-02-13 10:41:58 +0800

    add image plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,4 +5,21 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  -- https://github.com/3rd/image.nvim
+  {
+    "3rd/image.nvim",
+    build = false,
+    event = "VeryLazy",
+    opts = {
+      backend = "kitty",
+      processor = "magick_cli",
+      integrations = {
+        markdown = {
+          clear_in_insert_mode = true,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+        },
+      },
+    },
+  },
 }
